9fdb0397aa91ebe0b03b799965a97b3bd0d627b7
Rannie Ollit <einnar82@gmail.com>
2018-04-19 10:36:10 +0800

    Models

--- app/Http/Controllers/PhotowordController.php
+++ app/Http/Controllers/PhotowordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Photoword;
 use Illuminate\Http\Request;
 
 class PhotowordController extends Controller
@@ -40,10 +41,10 @@ class PhotowordController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Photoword  $photoword
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Photoword $photoword)
     {
         //
     }
@@ -51,10 +52,10 @@ class PhotowordController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Photoword  $photoword
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Photoword $photoword)
     {
         //
     }
@@ -63,10 +64,10 @@ class PhotowordController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Photoword  $photoword
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Photoword $photoword)
     {
         //
     }
@@ -74,10 +75,10 @@ class PhotowordController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Photoword  $photoword
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Photoword $photoword)
     {
         //
     }
---
+++ database/migrations/2018_04_19_022656_create_fix_it_ups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFixItUpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixitup', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('jumbled');
+            $table->string('answer');
+            $table->string('explanation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fix_it_ups');
+    }
+}
